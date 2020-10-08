4d63fb47cac945bc2dac87ab355c26a8170860d0
BrunoIbide <58187576+BrunoIbide@users.noreply.github.com>
2020-10-08 14:32:22 -0300

    php

---
+++ PHP-Exercicio Aula2/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aula 2</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<style type="text/css">
+		
+		.topo{
+			color: white;
+			padding-top: 10px;
+			background-image: linear-gradient(to right, #2E64FE, #08088A);
+			width: 100%;
+			height: 100px;
+			text-align: center;
+		}
+		.topo h1{
+			font-family: OCR A Std, monospace;
+		}
+		.topo2{
+
+		}
+		.navbar-expand-sm{
+			background-color: #170B3B;
+		}
+		.body1{
+			border-style: solid 1px;
+			padding-top: 15px;
+			padding-left: 1px;
+
+		}
+		.body1 a{
+			font-family: Snell Roundhand, cursive;
+			color: black;
+
+		}
+		.body1 ol{
+			
+		}
+		.body2 p{
+			padding-left: 15px;
+			padding-right: 15px;
+		}
+		.body table th{
+			text-align: center;
+		}
+		#imagem{
+		}
+		.body3 p{
+			padding-left: 15px;
+			padding-right: 15px;
+		}
+		.a a{
+			font-family: Snell Roundhand, cursive;
+			color: black;
+		}
+		.a a:link{
+			text-decoration: none;
+		}
+		.marvel{
+			padding-left: 15px;
+			padding-bottom: 15px;
+		}
+		.marvel ul{
+			padding-left: 600px;
+			font-family: Snell Roundhand, cursive;
+		}
+	</style>
+</head>
+<body>
+	<div class="topo">
+	<a id="topo"></a>
+		<h1>S.H.I.E.L.D</h1>
+	</div>
+
+	<div class="topo2">
+		
+	</div>
+	 <nav class="navbar navbar-expand-sm">
+  	  <img src="images/insta.png" width="50" height="50"><a class="navbar-brand" href="https://www.instagram.com/" target="_blank">&nbsp;Instagram</a>
+  		<ul class="navbar-nav">
+    	<li class="nav-item active">
+      <a class="nav-link" href="https://www.facebook.com/" target="_blank">Facebook</a>
+    		</li>
+    	<li class="nav-item">
+      <a class="nav-link" href="https://www.linkedin.com/feed/" target="_blank">Linkedin</a>
+    		</li>
+    	<li class="nav-item">
+      <a class="nav-link" href="https://www.youtube.com/" target="_blank">Youtube</a>
+    		</li>
+
+    	  </ul>
+
+	</nav> 
+	<div class="body1">
+	<ol>
+		<a href="#texto"><li>O que é?</li></a>
+		<a href="#herois"><li>Heróis</li></a>	
+	</ol>
+	<hr>
+	</div>
+	<div class="body2">
+		<p align="justify"><font face = "Arial" size="3" color="black" id="texto">
+			É uma super organização, muito mais forte e importante do que qualquer outra (CIA, NSA, FBI, etc). Com uma mega base invisível e flutuante liderada por Nick Fury que cuida dos interesses dos EUA e do mundo. E ainda monitora os heróis (em especial os Vingadores) em suas missões, além de ser considerada a "última defesa do mundo" contra ameças superiores, como os Skrulls, a Hydra, Inumanos, etc. 
+			<br><br>
+			A S.H.I.E.L.D. foi fundada por Howard Stark e Peggy Carter entre 1948 e 1950 depois da segunda guerra mundial como resposta a descoberta das operações da terrível organização subversiva internacional conhecida como Hydra. Para seu diretor de operações foi convocado o ex-soldado americano e ex-agente da CIA, coronel Nicholas (Nick) Joseph Fury.
+			<br><br>
+			Embora seja uma organização internacional, sem vínculos legais com qualquer nação, a maior parte dos agentes e do financiamento da organização partem dos EUA - o que a coloca sob grande influência desse país. É, por isso, motivo de críticas e discussões já por muitos anos o papel de "agente do imperialismo americano" da S.H.I.E.L.D. 
+		</font></p>
+		<center><a href="https://pt.wikipedia.org/wiki/S.H.I.E.L.D."target="_blank"><img src="images/shield.png"></center></a>
+		<hr>
+		<br>
+	
+		<table border="0" width="100%" id="herois">
+		<tr>
+			<center><th colspan="3" bgcolor="#2E9AFE" style="padding-left: 600px;">Principais Heróis</th></center>
+		</tr>
+		<tr>
+			<td>Homem de Ferro</td>
+			<td>Capitão América</td>		
+			<td>Thor</td>
+		</tr>
+		<tr>
+			<td>Viúva Negra</td>
+			<td>Gavião Arqueiro</td>		
+			<td>Hulk</td>
+		</tr>
+		<tr>
+			<td>Senhor das Estrelas</td>
+			<td>Homem Formiga</td>		
+			<td>Groot</td>
+		</tr>
+	</table>
+	<hr>
+</div>
+	<br><br><br>
+	<div id="imagem">
+		<center><img src="images/ironman.png" width=400" height="300">
+		<img src="images/thor.png" width="400" height="300">
+		<img src="images/capitao.png" width="400" height="300"></center>
+		<center><img src="images/viuva.png" width=400" height="300">
+		<img src="images/gaviao.png" width="400" height="300">
+		<img src="images/hulk.png" width="400" height="300"></center>
+		<center><img src="images/estrela.png" width=400" height="300">
+		<img src="images/formiga.png" width="400" height="300">
+		<img src="images/groot.png" width="400" height="300"></center>
+		
+	</div>
+<hr>
+	<div class="body3">
+		<p>A principal base da S.H.I.E.L.D. está localizada num gigantesco aeroporta-aviões e a agência possuiu escritórios regionais e bases secretas em todas as grandes cidades do ocidente. Embora a maior parte das ações da organização seja secreta sua existência é de conhecimento público, sendo considerado o órgão máximo de manutenção da lei (bem exemplificado na saga Operação: Tolerância Zero, quando eles inutilizam a força-tarefa multinacional elaborada para acabar com a "Ameaça Mutante)". </p>
+		<center><img src="images/aviao.png" width="1000" height="500"></center>
+	</div>
+	<br>
+	
+<div class="a">	
+<center><a href="#topo"><button type="button" class="btn btn-primary">Clique para ir ao topo da página</button>
+		
+	</a></center>
+</div>
+<hr>
+	<div class="marvel">
+		<ul>
+		<li>Site de Fã para Fã</li>
+		<li>Proibido Cópias</li>
+	</ul>
+		<a href="https://www.marvel.com/" target="_blank"><img src="images/logomarvel.png" width="200" height="80"></a>
+	
+	</div>
+</body>
+</html>
